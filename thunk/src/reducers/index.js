3d546ad9e01218e18f497410b1c7044ae8d526e0
Michaiah Bos <michaiahbos@yahoo.com>
2024-01-02 11:59:05 -0600

    changed initialstate in index.js

--- thunk/src/reducers/index.js
+++ thunk/src/reducers/index.js
@@ -1,18 +1,7 @@
 const initialState = {
-  person: {
-    name: {
-      title: "Mr",
-      first: "Silas",
-      last: "Petersen"
-    },
-    picture: {
-      large: "https://randomuser.me/api/portraits/men/70.jpg",
-      medium: "https://randomuser.me/api/portraits/med/men/70.jpg",
-      thumbnail: "https://randomuser.me/api/portraits/thumb/men/70.jpg"
-    }
-  },
-  isFetching: false,
-  error: ''
+ quote: '',
+ isFetching: false,
+ error: ''
 };
 
 export const reducer = (state = initialState, action) => {
