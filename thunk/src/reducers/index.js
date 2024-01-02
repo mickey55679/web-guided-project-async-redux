3c6a4799d94a77336160a01910e54af370477e92
Michaiah Bos <michaiahbos@yahoo.com>
2024-01-02 12:09:50 -0600

    had to revert back to original

--- thunk/src/reducers/index.js
+++ thunk/src/reducers/index.js
@@ -1,7 +1,18 @@
 const initialState = {
- quote: '',
- isFetching: false,
- error: ''
+person: {
+  name: {
+    title: 'Mr',
+    first: 'Silas',
+    last: 'Petersen'
+  },
+  picture: {
+    large: 'https://randomuser.me/api/portraits/men/70.jpg',
+    medium: 'https://randomuser.me/api/portraits/med/men/70.jpg',
+    thumbnail: 'https://randomuser.me/api/portraits/thumb/men/70.jpg'
+  }
+},
+isFetching: false,
+error: ''
 };
 
 export const reducer = (state = initialState, action) => {
